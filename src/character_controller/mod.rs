af9e180c8d58509a3ef62cb52df067ab1518b311
Jan Hohenheim <jan@hohenheim.ch>
2025-11-13 11:01:45 +0100

    WIP

--- src/character_controller/mod.rs
+++ src/character_controller/mod.rs
@@ -3,12 +3,12 @@ use bevy::prelude::*;
 
 mod fixed_update_util;
 mod input;
-mod movement;
+mod quake_1;
 
 pub(crate) use input::{AccumulatedInput, Jump, Movement};
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins((fixed_update_util::plugin, input::plugin, movement::plugin))
+    app.add_plugins((fixed_update_util::plugin, input::plugin, quake_1::plugin))
         .configure_sets(
             FixedPostUpdate,
             CharacterControllerSystems::ApplyMovement.in_set(PhysicsSystems::First),
@@ -20,6 +20,8 @@ pub(super) fn plugin(app: &mut App) {
 #[require(AccumulatedInput, CharacterControllerState, RigidBody = RigidBody::Kinematic)]
 pub(crate) struct CharacterController {
     pub(crate) speed: Vec2,
+    pub(crate) air_speed: f32,
+    pub(crate) acceleration: f32,
     pub(crate) max_speed: f32,
     pub(crate) gravity: f32,
 }
@@ -27,9 +29,11 @@ pub(crate) struct CharacterController {
 impl Default for CharacterController {
     fn default() -> Self {
         Self {
-            speed: vec2(10., 8.),
-            max_speed: 20.0,
-            gravity: 9.81,
+            speed: vec2(10., 9.),
+            air_speed: 0.7,
+            acceleration: 0.25,
+            max_speed: 8.0,
+            gravity: 20.,
         }
     }
 }
@@ -38,7 +42,6 @@ impl Default for CharacterController {
 #[reflect(Component)]
 pub(crate) struct CharacterControllerState {
     pub(crate) grounded: Option<Entity>,
-    pub(crate) velocity: Vec3,
 }
 
 #[derive(SystemSet, Clone, Copy, Debug, PartialEq, Eq, Hash)]
--- src/character_controller/quake_1.rs
+++ src/character_controller/quake_1.rs
@@ -1,3 +1,4 @@
+use avian3d::prelude::LinearVelocity;
 use bevy::prelude::*;
 
 use crate::character_controller::{
@@ -26,6 +27,7 @@ fn run_kcc(
             &CharacterController,
             &CharacterControllerState,
             &AccumulatedInput,
+            &LinearVelocity,
         )>,
     >,
     mut input_scratch: Local<Vec<KccInput>>,
@@ -33,26 +35,27 @@ fn run_kcc(
     let dt = world.resource::<Time>().delta_secs();
     input_scratch.extend(
         kccs.iter(world)
-            .map(|(entity, cfg, state, input)| KccInput {
+            .map(|(entity, cfg, state, input, vel)| KccInput {
                 entity,
                 cfg: *cfg,
                 state: *state,
                 input: *input,
                 dt,
+                velocity: **vel,
             }),
     );
     for input in input_scratch.drain(..) {
-        let state: CharacterControllerState = match world.run_system_cached_with(air_move, input) {
+        let velocity: Vec3 = match world.run_system_cached_with(air_move, input) {
             Ok(state) => state,
             Err(err) => {
                 error!("Error running air_move system: {}", err);
                 continue;
             }
         };
-        *world
+        **world
             .entity_mut(input.entity)
-            .get_mut::<CharacterControllerState>()
-            .unwrap() = state;
+            .get_mut::<LinearVelocity>()
+            .unwrap() = velocity;
     }
 }
 
@@ -62,20 +65,20 @@ struct KccInput {
     cfg: CharacterController,
     state: CharacterControllerState,
     input: AccumulatedInput,
+    velocity: Vec3,
     dt: f32,
 }
 
-fn air_move(
-    In(KccInput {
+fn air_move(In(inp): In<KccInput>, world: &mut World) -> Result<Vec3> {
+    let KccInput {
         entity,
         cfg,
-        mut state,
+        state,
         input,
         dt,
-    }): In<KccInput>,
-    world: &mut World,
-) -> Result<CharacterControllerState> {
-    let dt = world.resource::<Time>().delta_secs();
+        mut velocity,
+    } = inp;
+
     let movement = input.last_movement.unwrap_or_default();
     let cfg_speed = cfg.speed.normalize_or_zero();
     let mut wish_vel =
@@ -87,30 +90,70 @@ fn air_move(
     }
 
     if state.grounded.is_some() {
-        state.velocity.z = 0.0;
-        accelerate();
-        state.velocity.z -= cfg.gravity * dt;
+        velocity.z = 0.0;
+        velocity = accelerate(wish_dir, wish_speed, velocity, cfg, dt);
+        velocity.z -= cfg.gravity * dt;
         ground_move();
     } else {
-        air_accelerate();
-        state.velocity.z -= cfg.gravity * dt;
+        velocity = air_accelerate(wish_dir, wish_speed, velocity, cfg, dt);
+        velocity.z -= cfg.gravity * dt;
         fly_move()
     }
-    Ok(state)
+    Ok(velocity)
 }
 
-fn accelerate() {
-    todo!();
+fn accelerate(
+    wish_dir: Dir3,
+    wish_speed: f32,
+    velocity: Vec3,
+    cfg: CharacterController,
+    dt: f32,
+) -> Vec3 {
+    let current_speed = velocity.dot(wish_dir.into());
+    // right here is where air strafing happens: `current_speed` is close to 0 when we want to move perpendicular to
+    // our current velocity, making `add_speed` large.
+    let add_speed = wish_speed - current_speed;
+    if add_speed <= 0.0 {
+        return velocity;
+    }
+
+    let accel_speed = (cfg.acceleration * dt * wish_speed).min(add_speed);
+    velocity + accel_speed * wish_dir
+}
+
+fn air_accelerate(
+    wish_dir: Dir3,
+    wish_speed: f32,
+    velocity: Vec3,
+    cfg: CharacterController,
+    dt: f32,
+) -> Vec3 {
+    let wish_speed = wish_speed.min(cfg.air_speed);
+    accelerate(wish_dir, wish_speed, velocity, cfg, dt)
 }
 
 fn ground_move() {
     todo!();
 }
 
-fn air_accelerate() {
+fn fly_move() {
     todo!();
 }
 
-fn fly_move() {
-    todo!();
+fn friction(
+    velocity: Vec3,
+    cfg: CharacterController,
+    state: CharacterControllerState,
+    dt: f32,
+) -> Vec3 {
+    let speed = velocity.length();
+    if speed < 0.025 {
+        return Vec3::ZERO;
+    }
+
+    // if the leading edge is over a dropoff, increase friction
+    if state.grounded.is_some() {
+        let start = todo!();
+    }
+    velocity
 }
