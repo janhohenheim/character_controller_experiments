8a90d0211065cde1479c775ebaac56baa6289829
Jan Hohenheim <jan@hohenheim.ch>
2025-11-16 14:59:35 +0100

    Tweak consts

--- src/character_controller/quake_3.rs
+++ src/character_controller/quake_3.rs
@@ -61,14 +61,14 @@ impl Default for CharacterController {
             min_walk_cos: 0.7,
             stop_speed: 5.0,
             friction_hz: 6.0,
-            speed: Vec2::new(7.0, 8.0),
+            speed: Vec2::new(7.0, 9.0),
             acceleration_hz: 10.0,
             air_acceleration_hz: 1.0,
             num_bumps: 4,
             gravity: 20.0,
             step_size: 1.0,
             max_slope_cosine: 0.7,
-            jump_speed: 15.0,
+            jump_speed: 8.0,
             crouch_scale: 0.25,
         }
     }
